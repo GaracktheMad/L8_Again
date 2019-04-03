55d8fa721492f69250c758aee2159ff244844a5f
Wyattmontana <Wyattmontana98@gmail.com>
2019-04-03 14:25:34 -0400

    Added Permissions as well as a way to format time/date
    Added permissions as well as a way to format time/date

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar c = Calendar.getInstance();
+        System.out.println("Current time => " + c.getTime());
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = df.format(c.getTime());
+
+        //formattedDate have current date/time
+        Toast.makeText(this, formattedDate, Toast.LENGTH_SHORT).show();
+
+
+        // display formattedDate value in TextView
+        TextView txtView = new TextView(this);
+        txtView.setText("Current Date and Time : " + formattedDate);
+        txtView.setGravity(Gravity.CENTER);
+        txtView.setTextSize(20);
+        setContentView(txtView);
     }
 }
