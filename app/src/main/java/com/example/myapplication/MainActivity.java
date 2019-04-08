1c26f5827d16f8f4277093bf68f13586644129bb
Wyattmontana <Wyattmontana98@gmail.com>
2019-04-08 09:21:46 -0400

    Something went wrong, trying to fix

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+        public void openAlarmScreenActivity(View view) {
+            Intent webIntent = new Intent(this, AlarmScreenActivity.class);
+            startActivity(webIntent);
+    }
+}
