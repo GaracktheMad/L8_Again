a01a470280d668392cc6e72d4c4f6a06d418f809
Wyattmontana <Wyattmontana98@gmail.com>
2019-04-08 09:07:48 -0400

    Button Functionalities
    Possible to move to seperate activities

--- app/src/main/java/com/example/myapplication/SettingPage.java
+++ app/src/main/java/com/example/myapplication/SettingPage.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingPage extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class SettingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_page);
+    }
+    public void openAlarmScreenActivity(View view) {
+        Intent webIntent = new Intent(this, AlarmScreenActivity.class);
+        startActivity(webIntent);
     }
 }
