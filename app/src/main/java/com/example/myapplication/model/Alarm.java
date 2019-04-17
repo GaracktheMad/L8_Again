68911aa7a05ac45949dababec0d6871f561930ae
bpeskett <bwptwo@gmail.com>
2019-04-16 20:57:46 -0400

    Alarm class

--- app/src/main/java/com/example/myapplication/model/Alarm.java
+++ app/src/main/java/com/example/myapplication/model/Alarm.java
@@ -1,4 +1,57 @@
 package com.example.myapplication.model;
 
-public class Alarm {
+public class Alarm
+{
+    private int dayOfWeek;
+    public int getDayOfWeek() {
+        return dayOfWeek;
+    }
+    public void setDayOfWeek(int dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    private int hour;
+    public int getHour(){
+        return hour;
+    }
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    private int minute;
+    public int getMinute() {
+        return minute;
+    }
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    private int alarmID;
+    public int getAlarmID() {
+        return alarmID;
+    }
+    public void setAlarmID(int alarmID) {
+        this.alarmID = alarmID;
+    }
+
+    private boolean amPm;
+    public boolean isAmPm() {
+        return amPm;
+    }
+    public void setAmPm(boolean amPm) {
+        this.amPm = amPm;
+    }
+
+    private boolean onOff;
+    public boolean isOnOff() {
+        return onOff;
+    }
+    public void setOnOff(boolean onOff) {
+        this.onOff = onOff;
+    }
+
+
+
+
+
 }
