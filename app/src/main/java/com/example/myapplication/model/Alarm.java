38ef0245d77e76dac729de463580092e2dc6b7d1
Peter Vukas <robodude55@hotmail.com>
2019-04-23 19:51:50 -0400

    Minor Alarm Change
    booleans baybee

--- app/src/main/java/com/example/myapplication/model/Alarm.java
+++ app/src/main/java/com/example/myapplication/model/Alarm.java
@@ -1,57 +1,69 @@
 package com.example.myapplication.model;
 
-public class Alarm
-{
-    private int dayOfWeek;
-    public int getDayOfWeek() {
-        return dayOfWeek;
+public class Alarm {
+    public Alarm(boolean amPm) {
+        this.amPm = amPm;
     }
-    public void setDayOfWeek(int dayOfWeek) {
-        this.dayOfWeek = dayOfWeek;
+
+    private boolean dayOfWeek[] = {false, false, false, false, false, false, false};
+
+    public boolean getDayOfWeekState(int day){
+        return dayOfWeek[day];
+    }
+
+    public void setDayOfWeek(int day, boolean state) {
+        this.dayOfWeek[day] = state;
     }
 
     private int hour;
-    public int getHour(){
+
+    public int getHour() {
         return hour;
     }
+
     public void setHour(int hour) {
         this.hour = hour;
     }
 
     private int minute;
+
     public int getMinute() {
         return minute;
     }
+
     public void setMinute(int minute) {
         this.minute = minute;
     }
 
     private int alarmID;
+
     public int getAlarmID() {
         return alarmID;
     }
+
     public void setAlarmID(int alarmID) {
         this.alarmID = alarmID;
     }
 
     private boolean amPm;
+
     public boolean isAmPm() {
         return amPm;
     }
+
     public void setAmPm(boolean amPm) {
         this.amPm = amPm;
     }
 
     private boolean onOff;
+
     public boolean isOnOff() {
         return onOff;
     }
+
     public void setOnOff(boolean onOff) {
         this.onOff = onOff;
     }
-
-
-
 
 
 }
